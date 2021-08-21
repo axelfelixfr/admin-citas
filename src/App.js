229ab241b1e787e53f151ef54d33ada7e1793b32
Axel <axelfelix123@hotmail.com>
2021-08-20 22:04:05 -0500

    Correccion de localstorage

--- src/App.js
+++ src/App.js
@@ -3,69 +3,59 @@ import Cita from './components/Cita';
 import Formulario from './components/Formulario';
 
 function App() {
+  // Obtenemos las citas en el localStorage con getItem
+  let citasIniciales = JSON.parse(localStorage.getItem('citas'));
 
-    // Obtenemos las citas en el localStorage con getItem
-    let citasIniciales = JSON.parse(localStorage.getItem('citas'));
-    
-    // Si no existe citasIniciales en el localStorage, entonces pasa a ser un arreglo vacío
-    if(!citasIniciales){
-        citasIniciales = [];
-    }
+  // Si no existe citasIniciales en el localStorage, entonces pasa a ser un arreglo vacío
+  if (!citasIniciales) {
+    citasIniciales = [];
+  }
 
-    // Arreglo de citas
-    const [citas, setCitas] = useState(citasIniciales); // El state inicial sera el de citasIniciales
+  // Arreglo de citas
+  const [citas, setCitas] = useState(citasIniciales); // El state inicial sera el de citasIniciales
 
-    // Función que tome las citas actuales y agregue la nueva
-    const crearCita = cita => {
-        // Al crear una cita usamos setCitas, se la pasamos al state citas, con el operador spread (...) pasamos el state anterior
-        setCitas([
-            ...citas,
-            cita
-        ]);
-    }
+  // Función que tome las citas actuales y agregue la nueva
+  const crearCita = cita => {
+    // Al crear una cita usamos setCitas, se la pasamos al state citas, con el operador spread (...) pasamos el state anterior
+    setCitas([...citas, cita]);
+  };
 
-    // Función que elimina una cita por su id
-    const eliminarCita = id => {
-        const citasRestantes = citas.filter(cita => cita.id !== id);
-        setCitas(citasRestantes);
-    }
+  // Función que elimina una cita por su id
+  const eliminarCita = id => {
+    const citasRestantes = citas.filter(cita => cita.id !== id);
+    setCitas(citasRestantes);
+  };
 
-    // useEffect para realizar ciertas operaciones cuando el state cambia
-    useEffect(() => {
-        // Si existen citasIniciales, agregamos el state citas al localStorage
-        if(citasIniciales){
-            localStorage.setItem('citas', JSON.stringify(citas));
-        } else {
-            localStorage.setItem('citas', JSON.stringify([]));
-        }
-    }, [citasIniciales, citas]);
+  // useEffect para realizar ciertas operaciones cuando el state cambia
+  useEffect(() => {
+    // Agregamos las citas, si no las hay se agregara un arreglo vacío []
+    localStorage.setItem('citas', JSON.stringify(citas));
+  }, [citasIniciales, citas]);
 
-    // Mensaje condicional sobre las citas
-    const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
+  // Mensaje condicional sobre las citas
+  const titulo = citas.length === 0 ? 'No hay citas' : 'Administra tus citas';
 
-    return (
-        <>
-            <h1>Administrador de Pacientes</h1>
+  return (
+    <>
+      <h1>Administrador de Pacientes</h1>
 
-            <div className="container">
-                <div className="row">
-                    <div className="one-half column">
-                        <Formulario crearCita={crearCita} />
-                    </div>
+      <div className="container">
+        <div className="row">
+          <div className="one-half column">
+            <Formulario crearCita={crearCita} />
+          </div>
 
-                    <div className="one-half column">
-                        <h2>{ titulo }</h2>
-                        {/* Iteramos cada una de las citas */}
-                        {
-                            citas.map(cita => (
-                                <Cita key={ cita.id } cita={ cita } eliminarCita={ eliminarCita } />
-                            ))
-                        }
-                    </div>
-                </div>
-            </div>
-        </>
-    );
+          <div className="one-half column">
+            <h2>{titulo}</h2>
+            {/* Iteramos cada una de las citas */}
+            {citas.map(cita => (
+              <Cita key={cita.id} cita={cita} eliminarCita={eliminarCita} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
